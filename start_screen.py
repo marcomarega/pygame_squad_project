5fb153545f9d8ee23fccad4053bd4d34f845bb05
Yudin_Artem <Yudinart12008@gmail.com>
2023-01-14 19:51:27 +1000

    start screen updated

--- start_screen.py
+++ start_screen.py
@@ -29,6 +29,7 @@ def load_image(name, colorkey=None):
 
 def checking_coords(coords):
     for btn in btns_coords.keys():
+        print(btn, coords)
         if btn[0] <= coords[0] <= btn[0] + btn[2] and btn[1] <= coords[1] <= btn[1] + btn[3]:
             return (True, btns_coords[btn])
     return (False,)
@@ -39,11 +40,51 @@ def terminate():
     sys.exit()
 
 
+def level_choosing(k):
+    intro_text = ["Выберите уровень",
+                  "1", "2", "3"]
+
+    fon_name_im = f'fon{k + 2}.jpg'
+    fon = pygame.transform.scale(load_image(fon_name_im), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        try:
+            color = 'white'
+            string_rendered = font.render(line, True, color)
+            intro_rect = string_rendered.get_rect()
+            line_int = int(line)
+            intro_rect.x = text_coord
+            intro_rect.y = 65
+            screen.blit(string_rendered, intro_rect)
+            pygame.draw.circle(screen, color, (intro_rect.x + 6, intro_rect.y + 8), 18, 2)
+            btns_coords[
+                (intro_rect.x - 3, intro_rect.y - 3, intro_rect.width + 3, intro_rect.height + 3)] = 3 + int(line)
+            text_coord += 40
+        except Exception:
+            text_coord = 25
+            intro_rect.x = 42
+            intro_rect.y = text_coord
+            screen.blit(string_rendered, intro_rect)
+            text_coord = 50
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                k = checking_coords(event.pos)
+                if len(k) == 2:
+                    return k[1]
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
 def start_screen(k):
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
+    intro_text = ["НАЗВАНИЕ ИГРЫ", "",
+                  "Играть"]
 
     fon_name_im = f'fon{k}.jpg'
     fon = pygame.transform.scale(load_image(fon_name_im), (WIDTH, HEIGHT))
@@ -52,15 +93,29 @@ def start_screen(k):
     text_coord = 50
     for line in intro_text:
         if k == 2:
-            string_rendered = font.render(line, True, pygame.Color('white'))
+            color = 'white'
         else:
-            string_rendered = font.render(line, True, pygame.Color('black'))
+            color = 'black'
+        string_rendered = font.render(line, True, color)
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
         intro_rect.x = 10
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
+        if line == 'Играть':
+            pygame.draw.rect(screen, color, (intro_rect.x - 3, intro_rect.y - 3,
+                                             intro_rect.width + 6, intro_rect.height + 6), 2)
+            btns_coords[
+                (intro_rect.x - 3, intro_rect.y - 3, intro_rect.width + 6, intro_rect.height + 6)] = 3
+    a = btns_coords.copy()
+    index = 0
+    for i in a.keys():
+        if index <= 1:
+            del btns_coords[i]
+            index += 1
+        else:
+            break
 
     while True:
         for event in pygame.event.get():
@@ -68,7 +123,10 @@ def start_screen(k):
                 terminate()
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
-                return
+                k = checking_coords(event.pos)
+                print(k)
+                if len(k) == 2 and k[1] == 3:
+                    return
         pygame.display.flip()
         clock.tick(FPS)
 
@@ -87,8 +145,8 @@ def choice():
     for line in text_for_btns:
         string_rendered = font.render(line, True, pygame.Color('white'))
         intro_rect = string_rendered.get_rect()
-        intro_rect.x = 120
-        intro_rect.y = text_coord
+        intro_rect.y = 120
+        intro_rect.x = text_coord
         screen.blit(string_rendered, intro_rect)
         pygame.draw.rect(screen, 'white', (intro_rect.x - 3, intro_rect.y - 3,
                                            intro_rect.width + 6, intro_rect.height + 6), 2)
@@ -111,3 +169,4 @@ def choice():
 
 k = choice()
 start_screen(k)
+level = level_choosing(k)
